c2a579e98f331655f2937ef51b084208e40db8ba
cf17jose.torres <cf17jose.torres@iesjoandaustria.org>
2019-12-16 19:58:30 +0100

    MainActivity.java : added Main2Activity.java : added activity_main.xml : added activity_main2.xml : added strings.xml : added drawable : added

--- app/src/main/java/com/example/gamepiedrapapeltijera/MainActivity.java
+++ app/src/main/java/com/example/gamepiedrapapeltijera/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gamepiedrapapeltijera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView Piedra, papel, tijera;
+
+        Piedra = findViewById(R.id.imgpiedra);
+        papel = findViewById(R.id.imgpapel);
+        tijera = findViewById(R.id.imgtijera);
+
+        Piedra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+        papel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tijera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
